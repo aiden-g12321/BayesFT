167c4cd78f6f10a5a67d4dbce294272d23208193
Lorenzo Speri <lorenzo.speri@gmail.com>
2025-02-20 10:09:55 +0100

    eryn mcmc

--- eryn_example/mcmc.py
+++ eryn_example/mcmc.py
@@ -20,6 +20,7 @@ from BayesFT import functions as f
 from BayesFT import hierarchical_models as hm
 from BayesFT.PTMCMC import PTMCMC
 from BayesFT import jumps
+from copy import deepcopy
 
 
 from eryn.ensemble import EnsembleSampler
@@ -39,7 +40,7 @@ t = jnp.linspace(t0, t1, Nt)
 # frequency bins injected into data (neglecting zero frequency)
 Nf_inj = t.shape[0] // 2
 
-model_1_Nf = 6  # number of frequency bins in model 1
+model_1_Nf = 10  # number of frequency bins in model 1
 Na_inj = 2 * Nf_inj # Aiden choice
 # Na_inj = model_1_Nf # consistent with model_1_Nf
 
@@ -58,12 +59,14 @@ diag_cov_inj = f.fast_diag_power_law_cov(log_amp_inj, gamma_inj, f_inj)
 a_seed = jr.PRNGKey(4)  # seed for injected Fourier coefficients
 np.random.seed(4)
 print('diag_cov_inj', diag_cov_inj.shape, "Na_inj", Na_inj)
-a_inj = np.sqrt(diag_cov_inj) * np.random.normal(0.0, size=(Na_inj,))#np.random.multivariate_normal(np.zeros_like(diag_cov_inj),np.diag(diag_cov_inj))# 
-a_inj[2*model_1_Nf:] = 0.0
-a_inj = jnp.array(a_inj)
+# a_inj = np.sqrt(diag_cov_inj) * np.random.normal(0.0, size=(Na_inj,)) # np.random.multivariate_normal(np.zeros_like(diag_cov_inj),np.diag(diag_cov_inj))# 
+# a_inj[2*model_1_Nf:] = 0.0
+# a_inj = jnp.array(a_inj)
 
 # shape of a_inj is (2*Nf_inj,)
-assert a_inj.shape[0] == Na_inj
+# assert a_inj.shape[0] == Na_inj
+a_inj = np.sqrt(diag_cov_inj) * jr.normal(a_seed, shape=(Na_inj,))#np.random.multivariate_normal(np.zeros_like(diag_cov_inj),np.diag(diag_cov_inj))# 
+
 # check accuracy of injection by averaging over many realizations
 many_a = np.sqrt(diag_cov_inj) * jr.normal(a_seed, shape=(10_000, Na_inj))
 
@@ -125,7 +128,73 @@ DEjumps_1 = jumps.DifferentialEvolution(100, model_1.x_min, model_1.x_max)
 jump_proposals_1 = [[Fisher_1.vectorized_Fisher_jump, 20],
                       [DEjumps_1.vectorized_DE_jump, 0]]
 
+from scipy.special import gamma
+
+class InverseGammaDistribution(object):
+    """Generate inverse gamma distribution with shape ``alpha`` and scale ``beta``
+
+    Args:
+        alpha (double): Shape parameter of the inverse gamma distribution
+        beta (double): Scale parameter of the inverse gamma distribution
+        use_cupy (bool, optional): If ``True``, use CuPy. If ``False`` use Numpy.
+            (default: ``False``)
+
+    Raises:
+        ValueError: Issue with inputs.
+
+    """
+
+    def __init__(self, alpha, beta, use_cupy=False):
+        if alpha <= 0 or beta <= 0:
+            raise ValueError("Alpha and beta must be positive.")
+
+        self.alpha = alpha
+        self.beta = beta
+
+        self.use_cupy = use_cupy
+        if use_cupy:
+            try:
+                cp.abs(1.0)
+            except NameError:
+                raise ValueError("CuPy not found.")
+
+    def rvs(self, size=1):
+        if not isinstance(size, int) and not isinstance(size, tuple):
+            raise ValueError("size must be an integer or tuple of ints.")
+
+        if isinstance(size, int):
+            size = (size,)
+
+        xp = np if not self.use_cupy else cp
+
+        out = 1 / xp.random.gamma(self.alpha, 1 / self.beta, size=size)
+
+        return out
+
+    def pdf(self, x):
+        xp = np if not self.use_cupy else cp
+
+        out = (self.beta ** self.alpha) / gamma(self.alpha) * x ** (-self.alpha - 1) * xp.exp(-self.beta / x)
+        out[x <= 0] = 0
+
+        return out
+
+    def logpdf(self, x):
+        xp = np if not self.use_cupy else cp
+
+        out = xp.zeros_like(x)
+        valid = x > 0
+        out[valid] = (self.alpha * xp.log(self.beta) - xp.log(gamma(self.alpha)) - (self.alpha + 1) * xp.log(x[valid]) - self.beta / x[valid])
+        out[~valid] = -xp.inf
+
+        return out
+
+    def copy(self):
+        return deepcopy(self)
+
 # ProbDistContainer
+inv = InverseGammaDistribution(1.0, 1.0)
+# breakpoint()
 priors = {
     "powerlaw": ProbDistContainer({
         0: uniform_dist(model_1.log_amp_min, model_1.log_amp_max),  # amplitude
@@ -134,12 +203,13 @@ priors = {
     "coeff": ProbDistContainer({
         ii: uniform_dist(model_1.a_min, model_1.a_max) for ii in range(model_1_Nf*2)}),
     "noise": ProbDistContainer({
-        0: uniform_dist(model_1.log_sigma_min, model_1.log_sigma_max),
+        # 0: uniform_dist(model_1.log_sigma_min, model_1.log_sigma_max),
+        0: inv
     }),
 }
 
 
-nwalkers = 30
+nwalkers = 64
 ntemps = 4
 ndims = {"powerlaw": 2, "coeff": model_1_Nf*2, "noise": 1}
 branch_names = ["powerlaw", "coeff", "noise"]
@@ -147,8 +217,11 @@ nleaves_max = {"powerlaw":  1, "coeff": 1, "noise": 1}
 nleaves_min = {"powerlaw":  1, "coeff": 1, "noise": 1}
 
 def loglike(x):
+    x[-1] = jnp.log(x[-1])
     inp = jnp.concatenate(x)
     res = model_1.fast_lnpost(inp)
+    if res == -jnp.inf:
+        return -1e50
     return res
 
 
@@ -196,8 +269,8 @@ log_like = ensemble.compute_log_like(coords, inds=inds, logp=log_prior)[0]
 
 # make sure it is reasonably close to the maximum which this is
 # will not be zero due to noise
-# print(log_like, log_prior)
-
+print(log_like, log_prior)
+# breakpoint()
 # setup starting state
 state = State(coords, log_like=log_like, log_prior=log_prior, inds=inds)
 nsteps = 10000
@@ -206,7 +279,7 @@ last_sample = ensemble.run_mcmc(state, nsteps, burn=1000, progress=True, thin_by
 print("acceptance rate: ", np.mean(ensemble.acceptance_fraction,axis=1))#, np.mean(ensemble.rj_acceptance_fraction,axis=1))    
 labels = {"powerlaw": ["log_amp", "gamma"], "coeff": [f"a_{i}" for i in range(model_1_Nf*2)], "noise": ["log_sigma"]}
 assert a_inj.shape[0] == Na_inj
-truths = {"powerlaw": hypers_inj, "coeff": a_inj[:model_1_Nf*2], "noise": [log_sigma_inj]}
+truths = {"powerlaw": hypers_inj, "coeff": a_inj[:model_1_Nf*2], "noise": [sigma_inj]}
 
 for br in branch_names:
     print("processing ", br)
